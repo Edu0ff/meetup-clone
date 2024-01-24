bb101ad3b431d05cf2e978f78c72aac821abd4ee
Ana <anabelenbernardez@gmail.com>
2024-01-24 20:07:41 +0100

    EventPage

--- frontend/src/components/EventCard/index.jsx
+++ frontend/src/components/EventCard/index.jsx
@@ -1,52 +1,59 @@
 import React from "react";
 import "./style.css";
 import Loading from "../Loading";
+import { Link } from "react-router-dom";
 
 function EventCard({ meetup }) {
   return (
-    <div className="event-card">
-      {meetup ? (
-        <>
-          <img
-            className="eventcard-image"
-            src={meetup.picture || ""}
-            alt={`Event: ${meetup.title}`}
-            width={300}
-            height={200}
-          />
-          <p className="eventcard-location">
+    <Link to={`/event/${meetup.id}`}>
+      <div className="event-card">
+        {meetup ? (
+          <>
             <img
-              className="event-icon"
-              src="icons/location.svg"
-              alt="Location"
+              className="eventcard-image"
+              src={meetup.picture || ""}
+              alt={`Event: ${meetup.title}`}
+              width={300}
+              height={200}
             />
-            {meetup.location}
-          </p>
-          <h1 className="eventcard-title">{meetup.title}</h1>
-          <p>{meetup.theme}</p>
-          <h2 className="eventcard-by">
-            <img
-              className="event-icon"
-              src="icons/person.svg"
-              alt="Organizer"
-            />
-            Organized by {meetup.organizer}
-          </h2>
-          <div className="eventcard-details">
-            <p className="eventcard-date">
-              <img className="event-icon" src="icons/calendar.svg" alt="Date" />
-              {meetup.date}
+            <p className="eventcard-location">
+              <img
+                className="event-icon"
+                src="icons/location.svg"
+                alt="Location"
+              />
+              {meetup.location}
             </p>
-            <p className="eventcard-going">
-              <img className="event-icon" src="icons/check.svg" alt="Going" />
-              {meetup.attendees_count} going
-            </p>
-          </div>
-        </>
-      ) : (
-        <p>No hay datos de eventos disponibles.</p>
-      )}
-    </div>
+            <h1 className="eventcard-title">{meetup.title}</h1>
+            <p>{meetup.theme}</p>
+            <h2 className="eventcard-by">
+              <img
+                className="event-icon"
+                src="icons/person.svg"
+                alt="Organizer"
+              />
+              Organized by {meetup.organizer}
+            </h2>
+            <div className="eventcard-details">
+              <p className="eventcard-date">
+                <img
+                  className="event-icon"
+                  src="icons/calendar.svg"
+                  alt="Date"
+                />
+                {meetup.date}
+              </p>
+              <p className="eventcard-going">
+                <img className="event-icon" src="icons/check.svg" alt="Going" />
+                {meetup.attendees_count} going
+              </p>
+            </div>
+          </>
+        ) : (
+          <p>No hay datos de eventos disponibles.</p>
+        )}
+      </div>
+    </Link>
   );
 }
 
