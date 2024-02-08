9a93006f693715b390ffcc0dbee3239f4c56ac23
Ana <anabelenbernardez@gmail.com>
2024-02-08 13:46:06 +0100

    First responsive styles for all components

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import HomeButton from "../HomeButton";
 import SignOutButton from "../SignOutButton";
+import { AuthContext } from "../../context/AuthContext.jsx";
 import "./style.css";
 
 function Header() {
+  const { token } = useContext(AuthContext);
+
   return (
     <header className="header">
       <div className="triangleright-header"></div>
@@ -13,14 +16,23 @@ function Header() {
         <div className="left-section">
           <HomeButton />
         </div>
-
+        <input
+          type="checkbox"
+          className="mobile-menu-check"
+          id="expand-menu"
+        ></input>
         <div className="right-section">
-          <NavLink to="/postevent">Post Event</NavLink>
+          {token && <NavLink to="/postevent">Post Event</NavLink>}
           <NavLink to="/events">Explore Events</NavLink>
           <div className="separator"></div>
           <div className="nav-signin">
             <SignOutButton />
           </div>
+        </div>
+        <div className="mobile-menu">
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
         </div>
       </nav>
     </header>
